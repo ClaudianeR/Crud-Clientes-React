ddc7b83410054417a7c1d3a20c0ddb1549b2dbea
claudianeR <claudiane.guapa@gmail.com>
2023-12-18 21:50:02 -0300

    crud

---
+++ CrudClientes/src/pages/clientes/index.jsx
@@ -0,0 +1,10 @@
+export function Clients(){
+    return(
+        <div>
+            <div className="flex justify-between items-center">
+            <h2>Clientes</h2>
+            <button className= "hover:bg-sky-600  px-2 py-1 rounded shadow-sm text-white uppercase">Novo cliente</button>
+            </div>  
+        </div>
+    );
+}
